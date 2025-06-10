80ba965efd2a6e5f565bf759572c43ffb2878618
ajaysathasivm <ajay555ko@gmail.com>
2025-06-10 22:04:18 +0530

    setting up onclick event

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
-    })
+        webPreferences: {
+            // preload: path.join(__dirname, 'preload.js') // Enable preload
+        }
+    });
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
