e886091bb5bf2465566d197834b837b5a2b9dfc1
ajaysathasivm <ajay555ko@gmail.com>
2025-06-11 22:40:59 +0530

    forge change

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,5 @@
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+const { FuseVersion, FuseV1Options } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
@@ -8,14 +8,6 @@ module.exports = {
   rebuildConfig: {},
   makers: [
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
       name: '@electron-forge/maker-deb',
       config: {},
     },
@@ -23,14 +15,16 @@ module.exports = {
       name: '@electron-forge/maker-rpm',
       config: {},
     },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['linux'],
+    }
   ],
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
       config: {},
     },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
     new FusesPlugin({
       version: FuseVersion.V1,
       [FuseV1Options.RunAsNode]: false,
